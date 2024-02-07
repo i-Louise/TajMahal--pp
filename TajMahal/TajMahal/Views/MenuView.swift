af59a374aa0b85176f62d7eeba2055a515b6940c
i-Louise <louisecindy.ta@gmail.com>
2024-02-07 13:23:35 +0100

    Added Menu screen and an asset for the pepper

--- TajMahal/TajMahal/Views/MenuView.swift
+++ TajMahal/TajMahal/Views/MenuView.swift
@@ -2,9 +2,10 @@
 //  MenuView.swift
 //  TajMahal
 //
-//  Created by Amandine Cousin on 31/10/2023.
+//  Created by Louise Ta on 06/02/2024.
 //
 
+import Foundation
 import SwiftUI
 
 // Menu sous forme de liste
@@ -13,12 +14,106 @@ struct MenuView: View {
     let viewModel: ViewModel = ViewModel()
     
     var body: some View {
-        List {
-            // À completer
+        VStack {
+            NavigationView {
+                List {
+                    Section {
+                        ForEach(viewModel.apetizerArray) { dish in
+                            DishCell(dish: dish)
+                        }
+                    } header: {
+                        Text("Entrées")
+                        // Opt-out from SwiftUI List section default header uppercase text style
+                            .textCase(nil)
+                            .bold()
+                        
+                    }
+                    
+                    // Create the second section for the main courses
+                    Section {
+                        ForEach(viewModel.mainCourseArray) { dish in
+                            DishCell(dish: dish)
+                        }
+                    } header: {
+                        Text("Plats Principaux")
+                            .textCase(nil)
+                            .bold()
+                    }
+                }
+                
+                .listRowSpacing(15.0)
+                .navigationTitle("Menu")
+                // hide the default larger title
+                .navigationBarTitleDisplayMode(.inline)
+
+            }
         }
     }
 }
 
-#Preview {
-    MenuView()
+struct DishCell: View {
+    let dish: Dish
+    
+    var body: some View {
+        HStack {
+            Image(dish.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 112, height: 86)
+                .cornerRadius(4)
+            VStack {
+                Text(dish.name)
+                    .font(.system(size: 14))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
+                    .frame(width: 0.0, height: 12.0)
+                Text(dish.description)
+                    .font(.system(size: 12))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
+                    .frame(width: 0.0, height: 12.0)
+                
+                HStack {
+                    Text(dish.price + "€")
+                        .font(.system(size: 12))
+                    Spacer()
+                    
+                    // Spice level icons
+                    if dish.spiceLevel == SpiceLevel.light {
+                        Image(.pepper)
+                            .renderingMode(.template)
+                            .foregroundColor(Color(.customRed))
+                        Image(.pepper)
+                        Image(.pepper)
+                    } else if dish.spiceLevel == SpiceLevel.medium {
+                        Image(.pepper)
+                            .renderingMode(.template)
+                            .foregroundColor(Color(.customRed))
+                        Image(.pepper)
+                            .renderingMode(.template)
+                            .foregroundColor(Color(.customRed))
+                        Image(.pepper)
+                    } else if dish.spiceLevel == SpiceLevel.hot {
+                        Image(.pepper)
+                            .renderingMode(.template)
+                            .foregroundColor(Color(.customRed))
+                        Image(.pepper)
+                            .renderingMode(.template)
+                            .foregroundColor(Color(.customRed))
+                        Image(.pepper)
+                            .renderingMode(.template)
+                            .foregroundColor(Color(.customRed))
+                    }
+                }
+            }
+        }
+    }
 }
+
+    
+
+    
+    #Preview {
+        MenuView()
+    }
+
