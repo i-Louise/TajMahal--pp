b59791abaed6a406f3c3e6dc9cac58aa0f202e1c
i-Louise <louisecindy.ta@gmail.com>
2024-02-08 01:24:01 +0100

    Created DishDetailView and started Ui

--- TajMahal/TajMahal/Views/DishDetailView.swift
+++ TajMahal/TajMahal/Views/DishDetailView.swift
@@ -6,3 +6,76 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct DishDetailView: View {
+    let viewModel: ViewModel = ViewModel()
+    let dish: Dish
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Image(dish.imageName)
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 335, height: 467)
+                    .cornerRadius(40)
+                                
+                HStack {
+                    if dish.spiceLevel == SpiceLevel.light {
+                        Image(.pepper)
+                            .renderingMode(.template)
+                            .foregroundColor(Color(.customRed))
+                        Image(.pepper)
+                        Image(.pepper)
+                    } else if dish.spiceLevel == SpiceLevel.medium {
+                        Image(.pepper)
+                            .renderingMode(.template)
+                            .foregroundColor(Color(.customRed))
+                        Image(.pepper)
+                            .renderingMode(.template)
+                            .foregroundColor(Color(.customRed))
+                        Image(.pepper)
+                    } else if dish.spiceLevel == SpiceLevel.hot {
+                        Image(.pepper)
+                            .renderingMode(.template)
+                            .foregroundColor(Color(.customRed))
+                        Image(.pepper)
+                            .renderingMode(.template)
+                            .foregroundColor(Color(.customRed))
+                        Image(.pepper)
+                            .renderingMode(.template)
+                            .foregroundColor(Color(.customRed))
+                    }
+                }
+                .padding(5.0)
+                .background(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 40, style: .continuous))
+            }
+            VStack(alignment: .leading) {
+                Spacer()
+                Text("Allergènes")
+                    .font(.system(size: 15))
+                Spacer()
+                Text(dish.allergens)
+                    .font(.system(size: 12))
+                Divider()
+                Text("Ingrédients:")
+                Spacer()
+                    .font(.system(size: 15))
+                Text(dish.ingredients)
+                    .font(.system(size: 12))
+                Spacer()
+                
+            }
+            .navigationTitle(dish.name)
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .padding(.horizontal)
+    }
+    
+}
+
+#Preview {
+    DishDetailView(dish: Dish(name: "Chicken Tikka Masala", description: "Poulet mariné, grillé et servi dans une sauce masala", allergens: "Lait, yaourt, beurre clarifié (ghee), crème fraîche, crème de coco, ail, oignon", ingredients: "Huile, beurre clarifié (ghee), oignon, ail, gingembre, poudre de curcuma, poudre de cumin, poudre de coriandre, piment en poudre, tomates en purée, crème fraîche, crème de coco, sel, coriandre fraîche", spiceLevel: .medium, imageName: "Tikka Masala", price: "13.00"))
+}
